05d356c61f88dfbc84276fe5bd76396be22c8fad
shubham2501 <mittal.shubh25@gmail.com>
2014-10-14 17:09:04 +0530

    Create plot4.R

---
+++ plot4.R
@@ -0,0 +1,22 @@
+dataset <- read.csv("household_power_consumption.txt", header=TRUE, sep=";")
+a <- dataset[dataset$Date=="1/2/2007",]
+a1 <- dataset[dataset$Date=="2/2/2007",] 
+data <- rbind(a,a1)
+data$Global_active_power <- as.numeric(data$Global_active_power)
+data$Global_reactive_power <- as.numeric(data$Global_reactive_power)
+data$day <- paste(data$Date,data$Time,sep=" ")
+data$day <- strptime(data$day,"%d/%m/%Y %H:%M:%S")
+par(mfrow = c(2, 2))
+with(data,{
+plot(data$day,data$Global_active_power/1000, type="l", xlab="",ylab="Global Active Power")
+plot(data$day,data$Voltage, type="l", xlab="datetime",ylab="Voltage")
+plot(data$day,data$Sub_metering_1, type="l", ylab="Energy sub metering", xlab="", ylim=range(c(data$Sub_metering_1,data$Sub_metering_2,data$Sub_metering_3)))
+par(new=TRUE)
+plot(data$day,data$Sub_metering_2,col="Red", type="l",axes=FALSE, xlab="", ylab="", ylim=range(c(data$Sub_metering_1,data$Sub_metering_2,data$Sub_metering_3)))
+par(new=TRUE)
+plot(data$day,data$Sub_metering_3,col="Blue", type="l",axes=FALSE, xlab="", ylab="", ylim=range(c(data$Sub_metering_1,data$Sub_metering_2,data$Sub_metering_3)))
+legend("topright", legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),pch="-",cex=0.7, col=c("Black","Red","Blue"))
+plot(data$day,data$Global_reactive_power/1000, type="l", xlab="datetime",ylab="Global_reactive_power")
+})
+dev.copy(png,"plot4.png",width=6,height=6,units="in",res=80)
+dev.off()
