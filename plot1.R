77f200456203d8f01bdf4cbe7f75d32b5538470e
shubham2501 <mittal.shubh25@gmail.com>
2014-12-08 02:01:14 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -3,6 +3,6 @@ a <- dataset[dataset$Date=="1/2/2007",]
 a1 <- dataset[dataset$Date=="2/2/2007",] 
 data <- rbind(a,a1)
 data$Global_active_power <- as.numeric(data$Global_active_power)
-hist(data$Global_active_power/1000, col="Red", xlab="Global Active Power (kilowatts)", main="Global Active Power")
+hist(data$Global_active_power, col="Red", xlab="Global Active Power (kilowatts)", main="Global Active Power")
 dev.copy(png,"plot1.png",width=4.8,height=4.8,units="in",res=100)
 dev.off()
