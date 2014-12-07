1342f4d6df6852add537d1c754a717b3b0679319
shubham2501 <mittal.shubh25@gmail.com>
2014-12-08 02:05:33 +0530

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,7 +1,7 @@
 dataset <- read.csv("household_power_consumption.txt", header=TRUE, sep=";")
-a <- dataset[dataset$Date=="1/2/2007",]
-a1 <- dataset[dataset$Date=="2/2/2007",] 
-data <- rbind(a,a1)
+a1 <- dataset[dataset$Date=="1/2/2007",]
+a2 <- dataset[dataset$Date=="2/2/2007",] 
+data <- rbind(a1,a2)
 data$day <- paste(data$Date,data$Time,sep=" ")
 data$day <- strptime(data$day,"%d/%m/%Y %H:%M:%S")
 plot(data$day,data$Sub_metering_1, type="l", ylab="Energy sub metering", xlab="", ylim=range(c(data$Sub_metering_1,data$Sub_metering_2,data$Sub_metering_3)))
